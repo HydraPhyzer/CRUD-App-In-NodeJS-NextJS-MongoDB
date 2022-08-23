d3591096eef959a1a85c835857415c83df176bb2
Muhammad Zubair <2020cs601@student.uet.edu.pk>
2022-08-23 12:47:16 +0500

    Added MongoDB Cluster Connection

--- Backend/DB/Connect.js
+++ Backend/DB/Connect.js
@@ -1,2 +1,2 @@
 let Mongoose=require('mongoose')
-let XYX=Mongoose.connect('mongodb://localhost:27017/CRUD')
+let XYX=Mongoose.connect('mongodb+srv://minecrudapp:minecrudapp123@cluster0.j2wknu4.mongodb.net/CRUD?retryWrites=true&w=majority')
